ed9273de6ceaf2e63d46583c2df60c717dfd110e
bakytzhan <bakytzhan.bektugan@gmail.com>
2019-08-05 16:22:48 +0600

    Added microblaze Arty 7 100-T echo server demo project

---
+++ Vivado_projects/mb_echo_server_demo/mb_server.sdk/echo_server_mb/Debug/src/platform_zynqmp.d
@@ -0,0 +1,1 @@
+src/platform_zynqmp.o src/platform_zynqmp.d: ../src/platform_zynqmp.c
---
+++ Vivado_projects/mb_echo_server_demo/mb_server.sdk/echo_server_mb/Debug/src/sfp.d
@@ -0,0 +1,4 @@
+src/sfp.o src/sfp.d: ../src/sfp.c \
+ ../../echo_server_mb_bsp/microblaze_0/include/xparameters.h
+
+../../echo_server_mb_bsp/microblaze_0/include/xparameters.h:
