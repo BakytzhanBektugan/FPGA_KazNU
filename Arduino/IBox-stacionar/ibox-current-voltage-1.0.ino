40dc29f4859cc3bf5167cc93036c6d3141134b58
Bakytzhan <bakytzhan.bektugan@gmail.com>
2020-02-08 19:47:05 +0600

    ibox-current-voltage-1.0.ino
    Emonlib current voltage

---
+++ Arduino/IBox-stacionar/ibox-current-voltage-1.0.ino
@@ -0,0 +1,24 @@
+#include "EmonLib.h"             
+#define VOLT_CAL 335.7
+#define CURRENT_CAL 11.11
+
+EnergyMonitor emon1;             // Create an instance
+
+void setup()
+{  
+  Serial.begin(9600);
+  
+  emon1.voltage(0, VOLT_CAL, 1.7);     // Voltage: input pin, calibration, phase_shift
+  emon1.current(1, CURRENT_CAL);       // Current: input pin, calibration.
+}
+
+void loop()
+{
+  emon1.calcVI(20,2000);         // Calculate all. No.of half wavelengths (crossings), time-out
+
+  float currentDraw = emon1.Irms;        //extract Irms into Variable
+  float supplyVoltage = emon1.Vrms;      //extract Vrms into Variable
+
+  Serial.println((String)"V=" + supplyVoltage + ";" + "I=" + currentDraw + ";");
+  delay(2000);
+}
