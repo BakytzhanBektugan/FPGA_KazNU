efb8f007b5f70969f87ee845caa1fe056058f8c5
Bakytzhan <bakytzhan.bektugan@gmail.com>
2020-02-09 15:22:21 +0600

    Added nano dht mq9-135

---
+++ Arduino/IBox-stacionar/nano_dht_co_ppm.ino
@@ -0,0 +1,14 @@
+#include "DHT.h" 
+#include <MQ135.h> 
+MQ135 gasSensor = MQ135(A0);
+DHT dht(2, DHT11);
+void setup()
+{
+    Serial.begin(9600);
+    dht.begin();
+}
+void loop() 
+{
+ Serial.print((String)"CO2="+gasSensor.getPPM()+";GAS_MQ2="+analogRead(A1)+";T="+dht.readTemperature()+";H="+dht.readHumidity()); // выдача в последовательный порт
+ delay(10000);
+}
